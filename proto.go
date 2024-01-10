591c2c46b38b84f57f6038ce506339a971149ac0
Neptune Tan <32198721+c3b2a7@users.noreply.github.com>
2024-01-10 17:35:28 +0800

    add comments

--- proto.go
+++ proto.go
@@ -1,5 +1,6 @@
 package main
 
+//go:generate go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@latest
 //go:generate go install -v google.golang.org/protobuf/cmd/protoc-gen-go@latest
 //go:generate go install -v google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 //go:generate go run ./tools/protogen/
